ed42157ec984c81489ad2c27a93896e68c8329a3
Gabriele <gabrielecervetti96@gmail.com>
2020-09-29 00:30:45 +0200

    more-corrections

--- js/_main.js
+++ js/_main.js
@@ -1,16 +1,11 @@
 
 $(document).ready(function () {
 
-    var users = [
-        'mario', 'giulio', 'alberto', 'claudia'
-    ]
-
     $(".send-comment").click(function () {
-        var commentText = $(".comment-text").val();
-
+		var nickText = $("#formGroupExampleInput").val();
+        var commentText = $("#formGroupExampleInput2").val();
         var commentContainer = $(".comments-container");
-        var commentRow = '<li class="list-group-item"><a href="#" class="badge badge-light">' + users[Math.round(Math.random(0, 3))
-        ] + '</a>' + commentText + '</li>';
+        var commentRow = '<li class="list-group-item"><a href="#" class="badge badge-light">' + '</a>' + nickText + " scrive: </br> " + commentText + '</li>';
 
         commentContainer.append(commentRow);
 
